1b06cff3b7ab5a0d6da77a8ad64c3acb580ef3eb
gusamasan@gmail.com <gusamasan@gmail.com@7ca5f601-1771-1e60-c5b2-710f1d3a4ea4>
2011-09-03 15:59:00 +0000

    Este archivo será eliminado en un futuro; se agregar al proyecto sólo para realizar pruebas de configuración

---
+++ trunk/Borrar.java
@@ -0,0 +1,10 @@
+﻿ALTER TABLE adjudicacion ADD COLUMN id_llamado INT;
+
+CREATE SEQUENCE producto_contrataciones_seq INCREMENT 1 MINVALUE -32768 MAXVALUE 32767 START -32768;
+CREATE TABLE producto_contrataciones(
+	codigo			SMALLINT PRIMARY KEY	DEFAULT nextval( 'producto_contrataciones_seq' )	,
+	codigo_contrataciones	TEXT UNIQUE		,
+	actual			BOOLEAN	DEFAULT FALSE	,
+	codigo_medicamento	BIGINT REFERENCES medicamento( codigo )
+);
+COMMENT ON TABLE producto_contrataciones IS 'Registra los datos de productos administrados por la Dirección Nacional de Contrataciones';
