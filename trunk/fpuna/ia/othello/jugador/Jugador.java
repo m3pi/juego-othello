ddf28ce36370803c0c3238c8096fef64382172af
gusamasan@gmail.com <gusamasan@gmail.com@7ca5f601-1771-1e60-c5b2-710f1d3a4ea4>
2011-09-23 03:11:11 +0000

    Inicio de la codificación de Jugadores

---
+++ trunk/fpuna/ia/othello/jugador/Jugador.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fpuna.ia.othello.jugador;
+
+/**
+ *
+ * @author gusamasan
+ */
+public interface Jugador{
+    public void jugar();
+}
