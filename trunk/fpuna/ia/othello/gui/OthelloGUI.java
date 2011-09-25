7ae7afde7b37066fd9397d95ec3cdcce19e2d7f3
gusamasan@gmail.com <gusamasan@gmail.com@7ca5f601-1771-1e60-c5b2-710f1d3a4ea4>
2011-09-25 20:37:07 +0000

    Amplicación de la sección de CONFIGURACIÓN y ajuste del Tablero

--- trunk/fpuna/ia/othello/gui/OthelloGUI.java
+++ trunk/fpuna/ia/othello/gui/OthelloGUI.java
@@ -10,6 +10,7 @@ import java.awt.event.KeyEvent;
 import javax.swing.*;
 
 import fpuna.ia.othello.gui.accion.AccionJuegoNuevo;
+import fpuna.ia.othello.gui.accion.AccionConfiguracionLista;
 
 import fpuna.ia.othello.Utils.Tablero;
 
@@ -21,6 +22,9 @@ public class OthelloGUI extends JFrame{
 // ----------------------------------------------------------------------
 
     private TableroGUI elTableroGUI;
+
+    private JTabbedPane pestanias;
+    
     //private Tablero tablero;
     
 // ----------------------------------------------------------------------
@@ -40,8 +44,15 @@ public class OthelloGUI extends JFrame{
     // -------------------------------------------------------------------
 
         this.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+        //this.setLayout( new GridLayout(1,1 ));
+        //this.setLayout( null );
 
-        this.setBounds( 100, 100, 650, 650 );
+        //this.setSize( 100, 100 );
+        java.awt.Dimension tamanio;
+
+        tamanio = new java.awt.Dimension( 650, 750 );
+        this.setPreferredSize( tamanio );
+        //this.setSize( 350, 350 );
         
         this.add( this.obtenerPestanias() );
         
@@ -52,32 +63,29 @@ public class OthelloGUI extends JFrame{
     }
 
     private JTabbedPane obtenerPestanias(){
-    // ----------------------------------------------------------------------
-
-        JTabbedPane pestanias;
+    // ----------------------------------------------------------------------        
 
         JComponent panelJuego;
         JComponent panelConfiguracion;
 
     // ----------------------------------------------------------------------
 
-        pestanias           = new JTabbedPane();
-        //pestanias.setSize( 300, 300 );
-        
-        panelJuego          = makeTextPanel("Panel #1");
+        pestanias           = new JTabbedPane();        
 
         panelJuego          = new JPanel();
         this.elTableroGUI      = new TableroGUI();
-
+        
         panelJuego.setLayout(new BoxLayout( panelJuego, BoxLayout.PAGE_AXIS));
+        //panelJuego.setLayout( new GridLayout( 3, 1 ) );
         panelJuego.add( this.elTableroGUI );
+        panelJuego.add( new JSeparator() );
         panelJuego.add( this.obtenerBotonNuevoJuevo() );
         
         pestanias.addTab("Juego", null, panelJuego ,"Does nothing");
 
         pestanias.setMnemonicAt(0, KeyEvent.VK_1);
 
-        panelConfiguracion = makeTextPanel("Panel #2");
+        panelConfiguracion = makeTextPanel("Configuración");
         pestanias.addTab("Configuración", null, this.obtenerPanelConfiguracion(),
                           "Definición de los parámetros del juego");
         pestanias.setMnemonicAt(1, KeyEvent.VK_2);        
@@ -94,41 +102,88 @@ public class OthelloGUI extends JFrame{
 
         panel   = new JPanel();
         //panel.setSize(400, 400 );
-        //panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
+        panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
+        //panel.setLayout( new GridLayout( 4, 2 ) );
 
-        javax.swing.JSplitPane jSplitPane1 = new javax.swing.JSplitPane();
-        jSplitPane1.setOrientation(javax.swing.JSplitPane.VERTICAL_SPLIT);
-        //jSplitPane1.remove( 0 );
-        jSplitPane1.remove( 0 );
-        jSplitPane1.remove( 0 );
-        jSplitPane1.setDividerSize(35);
+        
+        JPanel panelJugadorUno   = this.obtenerOpcionesJugador( "Jugador (1)");
+        //panelJugadorUno.setLayout( new BoxLayout( panelJugadorUno, BoxLayout.Y_AXIS ) );
+        //panelJugadorUno.add( new JLabel("Jugador(1)") );
+        //panelJugadorUno.add( separador);
+        //panelJugadorUno.add( this.obtenerOpcionesJugador() );
 
-        JPanel panelAlgoritmo   = new JPanel();
-        panelAlgoritmo.setLayout( new BoxLayout( panelAlgoritmo, BoxLayout.Y_AXIS ) );
-        panelAlgoritmo.add( new JLabel("Algoritmo") );
-        JSeparator separador   = new JSeparator(SwingConstants.HORIZONTAL );
-        separador.setSize( 50, 50 );
-        panelAlgoritmo.add( separador);
-        panelAlgoritmo.add( this.obtenerOpcionesAlgoritmo() );
+        JPanel panelJugadorDos   = this.obtenerOpcionesJugador( "Jugador (2)");
+        //panelJugadorDos.setLayout( new BoxLayout( panelJugadorDos, BoxLayout.Y_AXIS ) );
+        //panelJugadorDos.add( new JLabel("Jugador(2)") );
+        //panelJugadorDos.add( separador2 );
+        //panelJugadorDos.add( this.obtenerOpcionesJugador() );
+        //panelJugadorDos = this.obtenerOpcionesJugador( "Jugador (2)");
+
+        JPanel panelAlgoritmo   = this.obtenerOpcionesAlgoritmo();
+        //panelAlgoritmo.setLayout( new BoxLayout( panelAlgoritmo, BoxLayout.Y_AXIS ) );
+        //panelAlgoritmo.add( new JLabel("Algoritmo") );
+        //panelAlgoritmo.add( separador3);
+        //panelAlgoritmo.add( this.obtenerOpcionesAlgoritmo() );
+
+        JPanel panelFicha   = this.obtenerOpcionesFichas();
+        //panelFicha.setLayout( new BoxLayout( panelFicha, BoxLayout.Y_AXIS ) );
+        //panelFicha.add( new JLabel("Ficha para Jugador(1)") );
+        //panelFicha.add( separador4);
+        //panelFicha.add( this.obtenerOpcionesFichas() );
 
         JPanel panelVarios   = new JPanel();
-        panelVarios.setLayout( new BoxLayout( panelVarios, BoxLayout.Y_AXIS ) );
+        //panelVarios.setLayout( new BoxLayout( panelVarios, BoxLayout.Y_AXIS ) );
+        panelVarios.setLayout( new GridLayout( 3, 1) );
         panelVarios.add(new JLabel("Varios"));
-        panelVarios.add(new JSeparator(SwingConstants.HORIZONTAL ));
-        panelVarios.add( this.obtenerOpcionesVarios() );
+        panelVarios.add( new JSeparator() );
+        panelVarios.add( this.obtenerOpcionesVarios() );        
 
-        jSplitPane1.add( panelAlgoritmo );
-        jSplitPane1.add( panelVarios );
-        //panel.add( panelAlgoritmo );
+
+        JPanel izquierda = new JPanel();
+        JPanel derecha = new JPanel();
+
+        izquierda.setLayout( new GridLayout(3, 1 ) );
+        derecha.setLayout( new GridLayout(2, 1 ) );
+
+        /*
+        izquierda.add( panelJugadorUno );
+        izquierda.add( panelJugadorDos );
+        izquierda.add( panelFicha );
+
+        derecha.add( panelAlgoritmo );
+        derecha.add( panelVarios );
+         *
+         */
+        
+        panel.add( panelJugadorUno );
+        panel.add( panelJugadorDos );
+        panel.add( panelFicha );
+        panel.add( panelAlgoritmo );        
+        panel.add( panelVarios );
+        panel.add( new JSeparator() );
+        panel.add( this.obtenerBotonConfiguracionLista() );
+        /*
+        jSplitPane1.add( izquierda );
+        jSplitPane1.add( derecha );
 
         panel.add( jSplitPane1 );
-        
-        //panel.add( panelVarios );
-        
-        
+        */
         return( panel );
     }
 
+
+    private JButton obtenerBotonConfiguracionLista(){
+    // ------------------------------------------------------------------------
+
+        JButton boton;
+
+    // ------------------------------------------------------------------------
+
+        boton   = new JButton( "Listo" );
+        boton.addActionListener( new AccionConfiguracionLista( this.pestanias ) );
+        
+        return( boton );
+    }
 
     /**
      * Retorn el botón "nuevo juego" cuya función es reiniciar el juego
@@ -144,13 +199,13 @@ public class OthelloGUI extends JFrame{
 
     // ----------------------------------------------------------------------
 
-        boton  = new JButton( "nuevo juego" );
+        boton  = new JButton( "jugar" );
         boton.addActionListener( new AccionJuegoNuevo( this.elTableroGUI ) );
 
         return( boton );
     }
 
-    public JComponent obtenerOpcionesAlgoritmo(){
+    public JPanel obtenerOpcionesAlgoritmo(){
     // ----------------------------------------------------------------------
 
         JPanel panel;
@@ -158,7 +213,9 @@ public class OthelloGUI extends JFrame{
     // ----------------------------------------------------------------------
         
         panel   = new JPanel();
-        panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
+        panel.setLayout( new GridLayout( 6, 1 ) );
+        //panel.setayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
+        //panel.setPreferredSize( new java.awt.Dimension( 150, 100 ) );
 
         JRadioButton minimax = new JRadioButton( "MiniMax" );
         minimax.setMnemonic(KeyEvent.VK_M );
@@ -178,12 +235,87 @@ public class OthelloGUI extends JFrame{
         group.add( alfaBeta     );
         group.add( aleatorio    );
 
+        panel.add( new JLabel( "Algoritmo" ) );
+        panel.add( new JSeparator() );
         panel.add( minimax      );
         panel.add( alfaBeta     );
         panel.add( aleatorio    );
+        panel.add( new JLabel( "" ) );
 
         return( panel );
     }
+
+    public JPanel obtenerOpcionesJugador( String etiqueta ){
+    // ----------------------------------------------------------------------
+
+        JPanel panel;
+
+    // ----------------------------------------------------------------------
+
+        JSeparator separador   = new JSeparator(SwingConstants.HORIZONTAL );
+        separador.setSize( 10, 10 );
+
+        panel   = new JPanel();
+        //panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
+        panel.setLayout( new GridLayout( 5, 1 ) );
+
+        JRadioButton jugadorHumano = new JRadioButton( "Humano" );
+        jugadorHumano.setMnemonic(KeyEvent.VK_O );
+        jugadorHumano.setActionCommand( "HUMANO" );
+        jugadorHumano.setSelected(true);
+
+        JRadioButton jugadorMaquina = new JRadioButton( "Máquina" );
+        jugadorMaquina.setMnemonic(KeyEvent.VK_I );
+        jugadorMaquina.setActionCommand( "MAQUINA" );
+
+        
+        ButtonGroup group = new ButtonGroup();
+        group.add( jugadorHumano    );
+        group.add( jugadorMaquina   );
+        
+
+        panel.add( new JLabel( etiqueta ) );
+        panel.add( separador );
+        panel.add( jugadorHumano      );
+        panel.add( jugadorMaquina     );
+        panel.add( new JLabel( "" ) );
+
+        return( panel );
+    }
+
+    public JPanel obtenerOpcionesFichas(){
+    // ----------------------------------------------------------------------
+
+        JPanel panel;
+
+    // ----------------------------------------------------------------------
+
+        panel   = new JPanel();
+        //panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );
+        panel.setLayout( new GridLayout( 5, 1 ) );
+
+        JRadioButton fichaNegra = new JRadioButton( "Negra" );
+        fichaNegra.setMnemonic(KeyEvent.VK_N );
+        fichaNegra.setActionCommand( "FICHA_NEGRA" );
+        fichaNegra.setSelected(true);
+
+        JRadioButton fichaBlanca = new JRadioButton( "Blanca" );
+        fichaBlanca.setMnemonic(KeyEvent.VK_B );
+        fichaBlanca.setActionCommand( "FICHA_BLANCA" );
+
+        ButtonGroup group = new ButtonGroup();
+        group.add( fichaNegra      );
+        group.add( fichaBlanca     );        
+
+        panel.add( new JLabel( "Ficha para Jugador (1) " ) );
+        panel.add( new JSeparator() );
+        panel.add( fichaNegra      );
+        panel.add( fichaBlanca     );
+        panel.add( new JLabel( "" ) );
+
+        return( panel );
+    }
+
 
     public JComponent obtenerOpcionesVarios(){
     // ----------------------------------------------------------------------
@@ -193,10 +325,11 @@ public class OthelloGUI extends JFrame{
     // ----------------------------------------------------------------------
 
         panel   = new JPanel();
-        //panel.setLayout( new BoxLayout( panel, BoxLayout.Y_AXIS ) );        
-        
+        panel.setSize( 150, 10 );
+        //panel.setLayout( new BoxLayout( panel, BoxLayout.X_AXIS ) );
+
         panel.add( new JLabel( "Profundidad" ) );
-        panel.add(new JTextField("Text field", 15));
+        panel.add(new JTextField( "3" , 15));
 
         return( panel );
     }
--- trunk/fpuna/ia/othello/gui/accion/AccionConfiguracionLista.java
+++ trunk/fpuna/ia/othello/gui/accion/AccionConfiguracionLista.java
@@ -3,9 +3,9 @@
 
 package fpuna.ia.othello.gui.accion;
 
-import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.*;
 
 import fpuna.ia.othello.gui.TableroGUI;
 
@@ -13,38 +13,26 @@ import fpuna.ia.othello.gui.TableroGUI;
  *
  * @author gusamasan
  */
-public class AccionJuegoNuevo implements ActionListener{
+public class AccionConfiguracionLista implements ActionListener{
 // ------------------------------------------------------------------------
 
-    private TableroGUI elTablero;
+    private JTabbedPane pestanias;
 
 // ------------------------------------------------------------------------
 
     /** Constructores ****************************************************/
-    public AccionJuegoNuevo( TableroGUI tablero ){
-        this.elTablero  = tablero;
+    public AccionConfiguracionLista( JTabbedPane unasPestanias ){
+        this.pestanias  = unasPestanias;
     }
     /*********************************************************************/
 
     public void actionPerformed( ActionEvent evento ){
     // ------------------------------------------------------------------------
 
-        JButton botonLlamador;
-
     // ------------------------------------------------------------------------
 
-        botonLlamador   = ( JButton )evento.getSource();
-
-        if( botonLlamador.getText().equals( "jugar" ) ){
-            botonLlamador.setText( "juego nuevo" );
-            this.elTablero.habilitarTablero();
-        }
-        else{
-            this.elTablero.refrescarTablerGUI();
-            this.elTablero.deshabilitarTablero();
-            
-            botonLlamador.setText( "jugar" );
-        }
+        this.pestanias.setSelectedIndex(0);
+        
     }
 
 }
