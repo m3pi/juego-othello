4348c3c1bb3885a7de6ac0340170fc4cf9fedf89
lamiamartinez <lamiamartinez@7ca5f601-1771-1e60-c5b2-710f1d3a4ea4>
2011-09-25 03:46:28 +0000

    Agrege algunas funciones en tablero que vamos a necesitar en el minimax, cambie en casilla fichas negras a -1 ya que estaba a 0, igual que transparente

--- fpuna/ia/othello/Utils/Tablero.java
+++ fpuna/ia/othello/Utils/Tablero.java
@@ -5,6 +5,9 @@
 
 package fpuna.ia.othello.Utils;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /**
  *
  * @author user
@@ -147,5 +150,129 @@ public class Tablero {
     public void setMatrizTablero(Casilla[][] matrizTablero) {
         this.matrizTablero = matrizTablero;
     }
-    
+
+
+
+    public boolean ponerFicha(Casilla cas) {
+   	  ArrayList pivotes;
+   	  Iterator it;
+          int icon =  cas.obtenerColorFicha();
+          
+
+   	  //comprobar que el destino está vacío
+   	  if (matrizTablero[cas.fila][cas.col].obtenerColorFicha() != Casilla.FICHA_TRANSPARENTE)
+   	     return false;
+
+   	  //buscar la/s ficha/s que hace/n de pivote
+   	  pivotes = crearListaPivotes(cas, icon);
+
+   	  //si no hay pivotes, el movimiento no es válido
+   	  if (pivotes.isEmpty())
+   	     return false;
+
+   	  //si hay pivotes, se deben cambiar de color las fichas entre la posición
+   	  //actual y cada uno de los pivotes
+   	  it = pivotes.iterator();
+   	  while (it.hasNext()) {
+   	     voltearFichas(cas, (Casilla)it.next(),icon);
+   	  }
+
+   	  //colocar la nueva ficha en el tablero
+          if (icon == Casilla.FICHA_BLANCA)
+              matrizTablero[cas.fila][cas.col].asignarFichaBlanca();
+          else
+               matrizTablero[cas.fila][cas.col].asignarFichaNegra();
+   	  return true;
+   }
+
+    private ArrayList crearListaPivotes(Casilla cas, int icon) {
+   	  ArrayList pivotes;
+   	  Casilla piv;
+   	  int incFila, incCol;
+
+   	  pivotes = new ArrayList();
+   	  //recorrer las 8 direcciones posibles
+   	  for (incFila=-1; incFila<=1; incFila++)
+   	     for (incCol=-1; incCol<=1; incCol++)
+   	        if ((incCol!=0)||(incFila!=0)) {
+   	           //buscar un pivote en esa dirección
+   	           piv = buscarPivote(cas, incFila, incCol, icon);
+   	           if (piv!=null)
+   	              pivotes.add(piv);
+   	        }
+
+      return pivotes;
+   }
+
+    private Casilla buscarPivote(Casilla cas, int incFila, int incCol, int icon) {
+   	  int f,c;
+
+          //a partir de dónde se empieza a buscar el pivote
+   	  f = cas.fila + incFila;
+   	  c = cas.col + incCol;
+
+   	  //no buscar el pivote fuera del tablero
+   	  if ((f<0)||(f>7)||(c<0)||(c>7))
+   	     return null;
+
+   	  //tiene que haber al menos una ficha del color contrario que "flanquear"
+   	  //entre la nueva ficha y el pivote
+   	  if ((matrizTablero[f][c].obtenerColorFicha() == Casilla.FICHA_TRANSPARENTE)||((matrizTablero[f][c].obtenerColorFicha()==icon)))
+   	     return null;
+
+   	  //buscar y devolver posición del pivote
+   	  while ((f>=0)&&(f<8)&&(c>=0)&&(c<8)) {
+         //hemos llegado a una casilla vacía, no hay pivote
+   	     if (matrizTablero[f][c].obtenerColorFicha() == Casilla.FICHA_TRANSPARENTE)
+   	        return null;
+   	     //pivote encontrado (ficha del mismo color)
+   	     if (matrizTablero[f][c].obtenerColorFicha()==icon)
+   	        return new Casilla(f,c);
+   	     //siguiente casilla
+             f += incFila;
+   	     c += incCol;
+   	  }
+
+   	  //no se ha encontrado pivote
+   	  return null;
+   }
+
+    private void voltearFichas(Casilla cas, Casilla pivote, int icon) {
+
+                  int incFila, incCol;
+                  int numFilas, numCols;
+                  int numVolteos;
+                  int i, f, c;
+
+                  numFilas = pivote.fila - cas.fila;
+                  numCols = pivote.col - cas.col;
+                  //ver la dirección del pivote con respecto a la nueva ficha
+                  if (numFilas!=0)
+                 incFila = numFilas/Math.abs(numFilas);
+              else
+                 incFila = 0;
+              if (numCols!=0)
+                 incCol = numCols/Math.abs(numCols);
+              else
+                 incCol = 0;
+
+              numVolteos = Math.max(Math.abs(numFilas), Math.abs(numCols))-1;
+              f = cas.fila;
+              c = cas.col;
+              //"dar la vuelta" a las fichas
+              for(i=0; i<numVolteos; i++)
+              {
+                 f += incFila;
+                 c += incCol;
+                 if(icon==Casilla.FICHA_BLANCA)
+                 {
+                     matrizTablero[f][c].asignarFichaBlanca();
+                 }
+                 if(icon==Casilla.FICHA_NEGRA)
+                 {
+                     matrizTablero[f][c].asignarFichaNegra();
+                 }
+              }
+   }
+
 }
