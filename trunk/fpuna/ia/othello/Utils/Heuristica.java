d688d298462528765fef942dafaf11181128dc71
lamiamartinez <lamiamartinez@7ca5f601-1771-1e60-c5b2-710f1d3a4ea4>
2011-09-28 02:23:22 +0000

    agregado algunas funciones para usar en minimax

--- trunk/fpuna/ia/othello/Utils/Heuristica.java
+++ trunk/fpuna/ia/othello/Utils/Heuristica.java
@@ -19,36 +19,36 @@ public class Heuristica {
 
 
     //Una heuristica posible a usar
-    public int h2(Tablero tablero,int playerColor)
+    public static int h2(Tablero tablero,int playerColor)
+    {
+        int score = Puntos(playerColor, tablero) - Puntos(-playerColor, tablero);
+
+        // If the game is over
+        if (tablero.EsFinalDeJuego())
         {
-            int score = Puntos(playerColor, tablero) - Puntos(-playerColor, tablero);
-
-            // If the game is over
-            if (tablero.GameOver())
-            {
-                // if player has won
-                if (score > 0)
-                    return 100;
-                // if player has lost (or tied)
-                else
-                    return -100;
-            }
-
-            // if game isn't over, return relative advatage
-            return score;
+            // if player has won
+            if (score > 0)
+                return 100;
+            // if player has lost (or tied)
+            else
+                return -100;
         }
 
-        public int Puntos(int playerColor, Tablero tablero)
-        {
-            int points = 0;
+        // if game isn't over, return relative advatage
+        return score;
+    }
 
-            for (int x = 0; x < Tablero.CANTIDAD_FILAS_DEFECTO; x++)
-                for (int y = 0; y < Tablero.CANTIDAD_COLUMNAS_DEFECTO; y++)
-                    if (tablero.getMatrizTablero()[x][y].obtenerColorFicha() == playerColor)
-                        points++;
+    public static int Puntos(int playerColor, Tablero tablero)
+    {
+        int points = 0;
 
-            return points;
-        }
+        for (int x = 0; x < Tablero.CANTIDAD_FILAS_DEFECTO; x++)
+            for (int y = 0; y < Tablero.CANTIDAD_COLUMNAS_DEFECTO; y++)
+                if (tablero.getMatrizTablero()[x][y].obtenerColorFicha() == playerColor)
+                    points++;
+
+        return points;
+    }
 
 
 
